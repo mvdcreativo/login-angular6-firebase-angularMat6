9477395aab720cdd82395dae3bce21a65bd9cef6
mvdcreativo <mvdcreativo@gmail.com>
2018-06-08 01:25:58 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'EM-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'EM';
+}
