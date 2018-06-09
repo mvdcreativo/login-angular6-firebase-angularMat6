6760a96e9388d0c12bd999fd149ba7032ae3dd4c
mvdcreativo <mvdcreativo@gmail.com>
2018-06-09 02:57:46 -0300

    Instalación de Firebase, AngularFire2 y Angular Material

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBjBn0g9lE45W_SscBi4SzrnVOrd9OAomE",
+    authDomain: "loginapp-34d93.firebaseapp.com",
+    databaseURL: "https://loginapp-34d93.firebaseio.com",
+    projectId: "loginapp-34d93",
+    storageBucket: "loginapp-34d93.appspot.com",
+    messagingSenderId: "654292414279"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBjBn0g9lE45W_SscBi4SzrnVOrd9OAomE",
+    authDomain: "loginapp-34d93.firebaseapp.com",
+    databaseURL: "https://loginapp-34d93.firebaseio.com",
+    projectId: "loginapp-34d93",
+    storageBucket: "loginapp-34d93.appspot.com",
+    messagingSenderId: "654292414279"
+  }
 };
 
 /*
